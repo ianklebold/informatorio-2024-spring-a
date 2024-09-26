5ea228aca02da9762b4552f11d825ce292856895
ianklebold <ianstgo@gmail.com>
2024-09-26 20:53:58 -0300

    Ejemplo query method

--- projectapp/src/main/java/com/info/app/projectapp/repository/proyecto/ProyectoRepository.java
+++ projectapp/src/main/java/com/info/app/projectapp/repository/proyecto/ProyectoRepository.java
@@ -3,7 +3,11 @@ package com.info.app.projectapp.repository.proyecto;
 import com.info.app.projectapp.domain.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, UUID> {
+
+    List<Proyecto> findByNombreLike(String nombre);
+
 }
